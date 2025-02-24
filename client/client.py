59340dd39d01c2d7db66685b741f4e962f577199
Adrian Criollo <adriancriollo@Adrians-MacBook-Pro.local>
2025-02-24 17:52:49 -0500

    updating

--- client/client.py
+++ client/client.py
@@ -1,15 +1,30 @@
 import socket
+import threading
+
+def handle_receive(client):
+    while True:
+        try:
+            data = client.recv(1024).decode()
+            if not data:  # Server disconnected
+                print("Server disconnected")
+                break
+            print(f"Server: {data}")
+        except:
+            print("Error receiving data")
+            break
+
+def handle_send(client):
+    while True:
+        try:
+            message = input("Enter message to send to server: ")
+            client.send(message.encode())
+        except:
+            print("Error sending data")
+            break
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 5000))
+client.connect(('127.0.0.1', 5000))  # Replace with the server's IP and port
 
-try:
-    while True:
-        message = input("Enter message to send to the server: ")
-        client.send(message.encode())
-        response = client.recv(1024).decode()
-        print(f"Server says: {response}")
-        if message.lower() == "exit":
-            break
-finally:
-    client.close()
+# Start threads for sending and receiving
+threading.Thread(target=handle_receive, args=(client,)).start()
+threading.Thread(target=handle_send, args=(client,)).start()
--- server/server.py
+++ server/server.py
@@ -1,21 +1,34 @@
 import socket
+import threading
+
+def handle_receive(client):
+    while True:
+        try:
+            data = client.recv(1024).decode()
+            if not data:  # Client disconnected
+                print("Client disconnected")
+                break
+            print(f"Client: {data}")
+        except:
+            print("Error receiving data")
+            break
+
+def handle_send(client):
+    while True:
+        try:
+            message = input("Enter message to send to client: ")
+            client.send(message.encode())
+        except:
+            print("Error sending data")
+            break
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('0.0.0.0', 5000))
+server.bind(('0.0.0.0', 5000)) 
 server.listen(1)
 
 print("Waiting for a connection...")
 client, addr = server.accept()
 print(f"Connected to {addr}")
 
-try:
-    while True:
-        data = client.recv(1024).decode()
-        if not data:
-            print("Client disconnected")
-            break
-        print(f"Received: {data}")
-        client.send("Acknowledged".encode())
-finally:
-    client.close()
-    server.close()
+threading.Thread(target=handle_receive, args=(client,)).start()
+threading.Thread(target=handle_send, args=(client,)).start()
